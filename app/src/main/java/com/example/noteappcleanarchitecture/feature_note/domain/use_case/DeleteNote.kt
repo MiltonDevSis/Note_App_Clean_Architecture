b87cfd647b20af9d1dede3f190521a7df796d4ba
MiltonDevSis <milton.dev.sis@gmail.com>
2021-09-27 19:48:27 -0300

    Delete use case

---
+++ app/src/main/java/com/example/noteappcleanarchitecture/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,13 @@
+package com.example.noteappcleanarchitecture.feature_note.domain.use_case
+
+import com.example.noteappcleanarchitecture.feature_note.domain.model.Note
+import com.example.noteappcleanarchitecture.feature_note.domain.repository.NoteRepository
+
+class DeleteNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(note: Note) {
+        repository.deleteNote(note)
+    }
+}
