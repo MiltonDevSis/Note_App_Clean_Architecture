f0683124223b4f09bfa82139dc0431901e228e0c
MiltonDevSis <milton.dev.sis@gmail.com>
2021-10-10 17:32:14 -0300

    Criado Add Edit Note view model

---
+++ app/src/main/java/com/example/noteappcleanarchitecture/feature_note/presentation/util/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.example.noteappcleanarchitecture.feature_note.presentation.util
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val ishintVisible: Boolean = true
+)
