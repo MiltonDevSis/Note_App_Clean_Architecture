c4b295385dcebff65c50044360bd5e32f3a99682
Milton P. Filho <miltondevsis@gmail.com>
2021-10-24 14:59:12 -0300

    Unit Tests

---
+++ app/src/test/java/com/example/noteappcleanarchitecture/feature_note/domain/use_case/GetNotesTest.kt
@@ -0,0 +1,9 @@
+package com.example.noteappcleanarchitecture.feature_note.domain.use_case
+
+import com.example.noteappcleanarchitecture.feature_note.data.repository.NoteRepositoryImpl
+import org.junit.Before
+
+class GetNotesTest {
+
+
+}
